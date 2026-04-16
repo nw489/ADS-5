4b46f8dbc2148550d608f73fbdffecd6f126d260
Letavin Egor <letavin123@yandex.ru>
2026-04-17 01:53:45 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,38 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[size];
+    int topIndex;
+ public:
+    TStack() : topIndex(-1) {}
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    bool isFull() const {
+        return topIndex == size - 1;
+    }
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::overflow_error("Stack overflow");
+        }
+        data[++topIndex] = value;
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        return data[topIndex--];
+    }
+    T get() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex];
+    }
 };
-
-#endif  // INCLUDE_TSTACK_H_
+#endif
